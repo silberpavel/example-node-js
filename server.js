ec2fb9cc2bd46f2242cbcb3a0743aad294d4535e
silberpavel <silber.ypm@gmail.com>
2018-01-27 20:10:22 +0500

    EJS + Bootstrap4

--- server.js
+++ server.js
@@ -1,47 +1,27 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 
-// app is server
 var app = express()
 
-var artists = [
-  {
-    id: 1,
-    name: "Metallica",
-  },
-  {
-    id: 2,
-    name: "Iron Maiden",
-  },
-  {
-    id: 3,
-    name: "Deep Purple",
-  },
-  {
-    id: 4,
-    name: Date.now(),
-  }
-]
- 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello API');              // .SEND responce
-})
+  res.sendFile(__dirname + "/index.html");              
+});
 
-app.get('/artists', function (req, res) {
-  res.send(artists);              
-})
+app.get('/about', function (req, res) {
+  res.sendFile(__dirname + "/about.html");       
+});
 
-app.get('/artists/:id', function (req, res) {
+app.get('/news/:id', function (req, res) {
+  var obj = {
+    title: "Last News", 
+    id: 5,
+    paragraphs: ['Simple', 'Regular text', 'Numbers', 88, 11]
+  };
+  res.render('news', { newsId: req.params.id, newParam: 8811, myObj: obj});   // lesson #14 Gosha
+});
 
-  console.log(req.params);  // log name of request after /
-
-  // in order to find element in array
-  var artist = artists.find(function(artist) {    
-    return artist.id === Number(req.params.id)
-  });
-  res.send(artist);              
-})
 
 
 app.listen(3083);
-console.log('API app started!');
+console.log('API app started! on port 3083');
