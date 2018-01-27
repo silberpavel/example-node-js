57b38d7f0235178e05037680b975ee673cedefd3
silberpavel <silber.ypm@gmail.com>
2018-01-27 12:48:03 +0500

    Get request example
    Get request example, from static array

--- express.js
+++ express.js
@@ -1,12 +1,41 @@
 var express = require('express')
+
+// app is server
 var app = express()
+
+var artists = [
+  {
+    id: 1,
+    name: "Metallica",
+  },
+  {
+    id: 2,
+    name: "Iron Maiden",
+  },
+  {
+    id: 3,
+    name: "Deep Purple",
+  }
+]
  
-
-
 app.get('/', function (req, res) {
-  res.send('html');
-  console.log(res.status.name);
+  res.send('Hello API');              // send responce
 })
 
-app.listen(3000);
-console.log('NOW, You are listening port 3000');
+app.get('/artists', function (req, res) {
+  res.send(artists);              
+})
+
+app.get('/artists/:id', function (req, res) {
+
+  console.log(req.params);  //name of request after /
+
+  var artist = artists.find(function(artist) {    // in order to find element in array
+    return artist.id === Number(req.params.id)
+  });
+  res.send(artist);              
+})
+
+
+app.listen(3083);
+console.log('API app started!');
